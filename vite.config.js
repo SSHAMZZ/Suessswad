5a4729c38f711550c44dd60a2782059cd4d7fb4a
Shamit Shetty <shamitshetty120@gmail.com>
2025-06-22 01:30:00 +0530

    final changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Suessswad/', // <--- your repo name!
   plugins: [react()],
+  base: '/', // 👈 change this from '/Suessswad/' to '/'
 })
